fb3cb777dbc6d1c56075045fd935c749f16d70f5
RomanStetsenko <rstetsenko@malkosua.com>
2015-03-12 18:59:54 +0200

    created layouts, first url connect test1

--- app/src/main/java/edu/my/rstetsenko/zenua/MainActivity.java
+++ app/src/main/java/edu/my/rstetsenko/zenua/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import edu.my.rstetsenko.zenua.fragments.ExchangeRateFragment;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,28 +14,26 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new ExchangeRateFragment())
+                    .commit();
+        }
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
